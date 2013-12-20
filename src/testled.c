8fa5c468c911554bd85eb68875912d4c1c1467a2
Ariel Burman <arielburman@gmail.com>
2013-12-20 20:48:14 -0300

    Agrego el codigo que funciona como test de los ojitos

--- src/testled.c
+++ src/testled.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -5,23 +6,28 @@
 #include "tlc5940.h"
 
 int main(void) {
-	// Enable Global Interrupts
 	TLC5940_Init();
+	
+#if (TLC5940_MANUAL_DC_FUNCS)
 	TLC5940_SetAllDC(63);
 	TLC5940_ClockInDC();
-  
-  
+#endif
+
 	// Default all channels to off
 	TLC5940_SetAllGS(0);
+	
+	// Enable Global Interrupts
 	sei();
 	
+	channel_t i = 0;
 	for (;;) {
-	  TLC5940_SetAllGS(0);
-   	  TLC5940_SetGSUpdateFlag();
-    _delay_ms(1000);
-	  TLC5940_SetAllGS(2055);
-   	  TLC5940_SetGSUpdateFlag();
-    _delay_ms(1000);
+		while(gsUpdateFlag);	// wait until we can modify gsData
+		TLC5940_SetAllGS(0);
+//		TLC5940_SetAllGS(4095);
+		TLC5940_SetGS(i, 4095);
+		TLC5940_SetGSUpdateFlag();
+		_delay_ms(100);
+		i = (i + 1) % numChannels;
 	}
 	
 	return 0;
