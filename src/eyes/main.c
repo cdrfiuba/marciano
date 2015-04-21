19630309c380ee5d0172681d35bea4ee2c7a66a7
Ariel Burman <arielburman@gmail.com>
2015-04-21 02:43:13 -0300

    actualizo las 3 placas, revisar y mandar a fabricar

---
+++ src/eyes/main.c
@@ -0,0 +1,66 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "eyes.h"
+#include "usart.h"
+
+#define CANT_MODES  10
+
+int main (void)
+{
+  _delay_ms(2000);
+  eyesInit();
+
+  USART0Setup(BAUD_PRESCALER(115200), USART_CHARSIZE_8BIT,
+    USART_PARITY_DIS, USART_STOP_1BIT, USART_MODE_ASYNC);
+
+  // Enable Interrupts
+  sei();
+
+  unsigned char letter;
+  uint8_t counter = 0 ;
+  setEyesAngry();
+  DDRC |= 1<<3;
+
+  for(;;) {
+    letter = USART0Receive();
+    if (letter == 'n') {
+      PINC |= 1<<3; //si recive una N, se invierte el led 0 de la placa (ademas el led 1 se invierte con cualquier dato nuevo).
+      if (counter < (CANT_MODES -1))
+        counter++;
+      else
+        counter = 0;
+    }
+    if (letter == 'p') {
+      if (counter > 0)
+        counter--;
+      else
+        counter = CANT_MODES -1;
+    }
+
+    counter = counter >= CANT_MODES ? 0 : counter;
+    counter = counter < 0 ? CANT_MODES-1 : counter;
+
+    if (counter == 0)
+      setEyesAngry();
+    if (counter == 1)
+      setEyesCircle();
+    if (counter == 2)
+      setEyesCircleMiddle();
+    if (counter == 3)
+      setEyesUpDown();
+    if (counter == 4)
+      setEyesBlink();
+    if (counter == 5)
+      setEyesWink_d();
+    if (counter == 6)
+      setEyesWink_i();
+    if (counter == 7)
+      setEyesTired();
+    if (counter == 8)
+      setEyesOn();
+    if (counter == 9)
+      setEyesOff();
+  }
+}
---
+++ src/remote_control/car_commands.h
@@ -0,0 +1,19 @@
+
+#ifndef __CAR_COMMANDS
+#define __CAR_COMMANDS
+
+#define START          1
+#define STOP           2
+#define NEXT_EYE_MODE  3
+#define PREV_EYE_MODE  4
+
+#define CENTER        10
+#define LEFT          20
+#define RIGHT         30
+
+#define FORWARD        0
+#define BACKWARD     100
+
+#endif
+
+
